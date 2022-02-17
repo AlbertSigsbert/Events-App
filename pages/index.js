9ad0054f7257cdf908ec1491007b1441d8b7a586
Albert <ishengomaalbert@gmail.com>
2022-02-17 10:51:49 +0300

    SSG for the Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
-
 import EventList from "../components/events/event-list";
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../components/helpers/util-api";
+import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function HomePage(props) {
-  const featuredEvents = getFeaturedEvents();
 
   return (
     <div>
       <h1 className="center">Featured Events</h1>
-       <EventList events={featuredEvents}/>
+      <EventList events={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: { events: featuredEvents },
+    revalidate: 10,
+  };
 }
 export default HomePage;
